a0b74e72b81d79df84862b37ff33a27ca323d86a
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-08-30 19:35:47 -0300

    feat: base header

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import { FaTooth } from "react-icons/fa";
+
+export const Logo = () => {
+  return (
+    <div className="flex items-center justify-center text-white gap-3">
+      <h1>Dr. JP</h1>
+      <FaTooth className="w-6 h-6"/>
+    </div>
+  );
+};
--- src/components/MenuItens/index.tsx
+++ src/components/MenuItens/index.tsx
@@ -1,15 +1,30 @@
 import { menuItens } from "@/data/menuItens";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export const MenuItens = () => {
-
   return (
-    <nav className="absolute flex flex-col items-center justify-center right-0 top-16 w-1/3">
+    <motion.nav
+      className="absolute flex flex-col items-center justify-center right-0 top-16 w-1/3 origin-top"
+      initial={{ scaleY: 0 }}
+      animate={{ scaleY: 1 }}
+      exit={{ scaleY: 0 }}
+      transition={{ duration: 0.3 }}
+    >
       <ul className="flex flex-col items-center justify-center bg-slate-600 w-full rounded-lg gap-6">
-        {
-          menuItens.map(item => <Link key={item.id} href={`/${item.slug}`}>{item.text}</Link>)
-        }
-      </ul> 
-    </nav>
+        {menuItens.map((item) => (
+          <Link key={item.id} href={`/${item.slug}`}>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1}}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, delay: 0.3 }}
+            >
+              {item.text}
+            </motion.span>
+          </Link>
+        ))}
+      </ul>
+    </motion.nav>
   );
 };
